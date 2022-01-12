564ae7838429cf382eb378cdccffe0a01bfa37f6
AgenteC <90355023+AgenteC@users.noreply.github.com>
2022-01-11 21:16:37 -0600

    Add files via upload

---
+++ alimentaAlperro/Plantilla-Proyecto-C36-main/Plantilla-Proyecto-C36-main/sketch.js
@@ -0,0 +1,97 @@
+var dog,sadDog,happyDog, database;
+var foodS,foodStock;
+var addFood;
+var foodObj;
+var feed,lastFed
+var FeedDog
+//crea aquí las variables feed y lastFed 
+
+
+function preload(){
+sadDog=loadImage("Dog.png");
+happyDog=loadImage("happy dog.png");
+}
+
+function setup() {
+  database=firebase.database();
+  createCanvas(1000,400);
+
+  
+
+  foodStock=database.ref('Food');
+  foodStock.on("value",readStock);
+  
+  dog=createSprite(800,200,150,150);
+  dog.addImage(sadDog);
+  dog.scale=0.15;
+
+  //crea aquí el boton Alimentar al perro
+  FeedDog=createButton("Alimeta al perro")
+  FeedDog.position(600,200)
+  FeedDog.mousePressed(feed)
+
+  addFood=createButton("Agregar Alimento");
+  addFood.position(800,95);
+  addFood.mousePressed(addFoods);
+
+}
+
+function draw() {
+  background(46,139,87);
+  foodObj.display();
+
+  //escribe el código para leer el valor de tiempo de alimentación de la base de datos
+  readStock(data);
+  
+ 
+  //escribe el código para mostrar el texto lastFed time aquí
+if(lastFed>=12){
+
+  text("Ultima hora que se alimento al perro :12AM")
+
+}else if(lastFed==0){
+  text("Ultima hora que se alimento al perro : 12PM",350,30);
+}else{
+
+}
+ //
+
+ 
+drawSprites();
+}
+//función para leer la Existencia de alimento
+function readStock(data){
+  foodS=data.val();
+  foodObj.updateFoodStock(foodS);
+}
+
+
+function feedDog(){
+ dog.addImage(happyDog)
+ if(foodObj.getFoodStock()<= 0){
+ foodObj.updateFoodStock(foodObj.getFoodStock()*0); 
+ }else{ foodObj.updateFoodStock(foodObj.getFoodStock()-1);
+ } 
+ database.ref('/').update({ Food:foodObj.getFoodStock(),
+ FeedTime:hour() })
+ }
+  //escribe el código aquí para actualizar las existencia de alimento, y la última vez que se alimentó al perro
+  var food_stock_val = foofObj.getFoodStock();
+  if(food_stock_val <= 0){
+  foodObj.updateFoodStock(food_stock_val *0);
+  }else{
+
+    foodObj.updateFoodStock(food_stock_val -1);
+
+  }
+
+  
+
+
+//funcón para agregar alimento al almacén
+function addFoods(){
+  foodS++;
+  database.ref('/').update({
+    Food:foodS
+  })
+}
